a61bd2b80af0541de150e33ed1d94b3a6d93346e
iamtaufik <muhtopik07@gmail.com>
2023-11-13 21:29:37 +0700

    initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+const PORT = process.env.PORT || 3000;
+
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/chat', (req, res) => {
+  res.render('index');
+});
+
+io.on('connection', (client) => {
+  console.log('New user connected');
+  client.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port http://localhost:${PORT}.`);
+});
